b8e55b7a38666e583b3183d05c424a218cdcfaf5
Paulo Duque <paulo.duque@terra.com.br>
2022-07-14 09:16:47 -0300

    meu projeto 1407

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
+
+  const nome = 'Terra Duque'
+
   return (
     <div className="App">
 
-      <h1> Olá REACT</h1>
-      <p> Meu Primeiro APP</p>
+      <SayMyName nome="Paulo Duque" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome='Rodrigo'
+        idade="28"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
+      />
 
     </div>
   );
